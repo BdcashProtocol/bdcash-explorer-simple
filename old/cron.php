4678a00ac78a803d81179aeb45814cec42e6b224
Fabianno Limma <fabiano.alexandrebiano@gmail.com>
2022-01-04 22:34:37 -0300

    delete old files

--- old/cron.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once('config.php');
-	
-	(PHP_SAPI === 'cli' || isset($_SERVER['HTTP_USER_AGENT'])) && die('This daemon can only run in CLI');
-	Daemon::init();
-	close();
-?>
--- old/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer navbar-fixed-bottom text-center">
-<p>Created by BDCash Team.</p>
-</footer>
-</body>
-</html>
-<?
-close();
-?>
